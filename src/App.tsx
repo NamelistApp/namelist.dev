07dacb1aa437bafb4bd20bb85bae1b5d509d7f9f
Davidmoreen <davidmoreen@gmail.com>
2025-01-15 11:20:06 -0500

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import { IconBrandGithub, IconCheck, IconInnerShadowBottom, IconMail } from '@tabler/icons-react'
+import { Button, Container, Highlight, Group, Image, List, Paper, SimpleGrid, Stack, Text, ThemeIcon, Title, rem, Center } from '@mantine/core'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Container size="lg">
+        <SimpleGrid cols={{ base: 1, sm: 2, lg: 2 }} mt={50} spacing="xl">
+          <Stack>
+            <AppLogo />
+            <Title order={1} mt={"md"}>
+              <Highlight fw={700} fz={50} highlight={"Open-Sourced"}>
+                The Open-Sourced CRM that Developers Love
+              </Highlight>
+            </Title>
+
+            <Text>
+              Namelist is a modern CRM built for developers using Laravel and React that you can self-host.
+            </Text>
+
+            <List
+              mt={30}
+              spacing="sm"
+              size="sm"
+              icon={
+                <ThemeIcon size={20} radius="xl">
+                  <IconCheck size={12} stroke={1.5} />
+                </ThemeIcon>
+              }
+            >
+              <List.Item>
+                <b>Object Management</b> - contact, company and list managment
+              </List.Item>
+              <List.Item>
+                <b>Email Support</b> - send 1:1 or bulk email to your objects
+              </List.Item>
+              <List.Item>
+                <b>Reports and Charts</b> - create and view reports and charts
+              </List.Item>
+              <List.Item>
+                <b>Forms and Waitlists</b> – start collecting user signups fast
+              </List.Item>
+            </List>
+
+            <Group mt={30}>
+              <Button component="a" href="mailto:davidmoreen@gmail.com?subject=namelist.dev" radius="xl" size="md" leftSection={<IconMail stroke={1.2} />}>
+                Get in touch!
+              </Button>
+              <Button component="a" href="https://github.com/NamelistApp/Namelist" variant="default" radius="xl" size="md" leftSection={<IconBrandGithub stroke={1.2} />}>
+                Source code
+              </Button>
+            </Group>
+          </Stack>
+          <Stack>
+            <Center>
+              <Paper radius="md" shadow="md" withBorder mt={50}>
+                <Image src="screenshot.png" radius="md" />
+              </Paper>
+            </Center>
+          </Stack>
+        </SimpleGrid>
+      </Container >
+    </>
+  )
+}
+
+function AppLogo() {
+  return (
+    <Group
+      gap="xs"
+      c="green"
+    >
+      <IconInnerShadowBottom style={{ width: rem(30), height: rem(30) }} stroke={1.5} />
+      <Title order={5} style={{ fontWeight: 500 }}>
+        Namelist
+      </Title>
+    </Group>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,48 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '@mantine/core/styles.css';
+import App from './App.tsx'
+import posthog from 'posthog-js'
+import { createTheme, colorsTuple, virtualColor, MantineProvider } from '@mantine/core'
+
+posthog.init('phc_xijaNsfdqfXXTDXPoweUcvEcpcP9qwaVcCd7MIgXCWO',
+  {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only'
+  }
+)
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Initial />
+  </StrictMode>,
+)
+
+function Initial(): JSX.Element {
+  const theme = createTheme({
+    fontFamily: '"Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif',
+    colors: {
+      'soft-sand': colorsTuple(
+        Array.from({ length: 10 }, (_, index) => '#fcfaf8')
+      ),
+      'soft-sand-accent': colorsTuple(
+        Array.from({ length: 10 }, (_, index) => '#f4f1ed')
+      ),
+      'accent': virtualColor({ name: 'accent', light: 'soft-sand-accent', dark: 'dark-7' }),
+      'bg': virtualColor({ name: 'bg', light: 'soft-sand', dark: 'dark-8' }),
+    },
+    components: {
+      Modal: {
+        defaultProps: {
+          radius: 'lg',
+        }
+      }
+    }
+  })
+
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="auto">
+      <App />
+    </MantineProvider >
+  )
+}
